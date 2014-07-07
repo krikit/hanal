238693e6252a7e16b1ffaea09aaeea21fd0a50b2
krikit <krikit@naver.com>
2014-07-07 22:48:36 +0900

    improve alignment for spoken corpus

--- pylib/sejong_align.py
+++ pylib/sejong_align.py
@@ -5,7 +5,7 @@
 """align syllables to morphemes in word(EoJeol)"""
 __author__ = 'krikit'
 __email__ = 'krikit@naver.com'
-__date__ = 'creation: 2014-06-17, modification: 2014-06-17'
+__date__ = 'creation: 2014-06-17, modification: 2014-07-07'
 __copyright__ = 'nobody. feel free to use, copy and modify'
 
 
@@ -19,6 +19,7 @@ import logging
 # for decomposing syllables into consonants and vowels
 # http://biohackers.net/wiki/PythonAndHangul#head-4788cfe894c55238b6133994c6ab356302f63ec8
 import hangul
+import sejong_corpus
 
 
 #############
@@ -83,6 +84,34 @@ _CORRECT_ALIGNS = set([
   '찮 하/XSA + 지/EC + 않/VX',
   '놨 놓/VX + 았/EP',
   '돼 되/XSV + 어/EF',
+  "’ '/SS + 이/VCP",
+  '마 말/VX + 아/EC',
+  '요 이/VCP + 오/EC',
+  '어째 어찌/MAG + 하/XSV + 아/EC',
+  '뭔 무엇/NP + 이/VCP + ᆫ/ETM',
+  '여 이/VCP + 어/EC',
+  '놔 놓/VV + 아/EC',
+  '줘 주/VV + 어/EF',
+  '∼ ~/SO',
+  '줘 주/VV + 어/EC',
+  '뭐 뭣/NP + 이/VCP',
+  '꿔 꾸/VV + 어/EC',
+  '쪄 찌/VV + 어/EC',
+  '쏴 쏘/VV + 아/EC',
+  '마 말/VV + 아/EF',
+  '줘 주/VX + 어/EC',
+  '랬 라/EC + 하/VV + 았/EP',
+  '셔 시/EP + 어/EF',
+  '랬 이/VCP + 라/EC + 하/VV + 았/EP',
+  '세요 이/VCP + 시/EP + 어요/EF',
+  '뭘로 무엇/NP + 으로/JKB',
+  '뭘까 무엇/NP + 이/VCP + ᆯ까/EF',
+  '돼 되/XSA + 어/EC',
+  '쟤 저/MM + 아이/NNG',
+  '둬 두/VV + 어/EC',
+  '테 터/NNB + (이)/VCP',
+  '해야 하/VV + ㅕ야/EC + (하)/VX',
+  '놔 놓/VV + ㅏ/EC',
 ])
 
 
@@ -139,7 +168,12 @@ def match_pfx(uni_word, morphs):
   if uni_word.startswith(uni_morph):    # just one morpheme starts with word
     return len(uni_morph), 1
   if len(morphs) == 1:    # only one morpheme is left
-    return 1, len(uni_word)
+    morph_dec = decompose(morphs[0].lex)
+    word_dec = decompose(uni_word)
+    if word_dec == morph_dec:
+      return 1, len(uni_word)
+    else:
+      return -1, -1
   for i in range(2, len(morphs)+1):
     submorphs = ''.join([morph.lex for morph in morphs[:i]])
     submorphs_dec = decompose(submorphs)
@@ -263,9 +297,147 @@ def calc_similarity(lhs, rhs):
   return sim
 
 
-def proc_sandwich(forward_pairs, uni_word, morphs, backward_pairs):
+def proc_sw_both_not_empty(forward_pairs, uni_word, morphs, backward_pairs):
+  """
+  process sandwich(remaining) (word, morphs) pair when both word and morpheme list are not empty
+  @param  forward_pairs   forward pair list
+  @param  uni_word        word
+  @param  morphs          morpheme list
+  @param  backward_pairs  backward pair list
+  @return                 sandwich pair list. AlignError raised when error occurs
+  """
+  sandwich_pairs = []
+  morphs_str = ' + '.join([str(morph) for morph in morphs])
+  word_morphs_str = '%s %s' % (uni_word.encode('UTF-8'), morphs_str)
+  if word_morphs_str in _CORRECT_ALIGNS:
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs))
+    del morphs[:]
+  elif word_morphs_str in ['해야 하/VV + 아야/EC + 하/VX', '해야 하/XSV + 아야/EC + 하/VX', '야 아야/EC + 하/VX',
+      '야 어야/EC + 하/VX', '봐야 보/VX + 아야/EC + 하/VX', '해야 하/VX + 아야/EC + 하/VX'] \
+      and backward_pairs and str(backward_pairs[0].morphs[0]) == '겠/EP':
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[:-1]))
+    backward_pairs[0].morphs.insert(0, morphs[-1])
+    del morphs[:]
+  elif word_morphs_str in ['졌 어/EC + 지/VX + 었/EP', '져 어/EC + 지/VX + 어/EC', '봐야 아/EC + 보/VX + 아야/EC',
+      '왔 아/EC + 오/VX + 았/EP', '왔 어/EC + 오/VX + 았/EP', '봤 아/EC + 보/VX + 았/EP', '봐 아/EC + 보/VX + 아/EF',
+      '왔 아/EC + 오/VV + 았/EP', '냈 아/EC + 내/VX + 었/EP', '내 아/EC + 내/VX + 어/EC'] \
+      and forward_pairs and forward_pairs[-1].morphs[-1].tag == 'VV':
+    forward_pairs[-1].morphs.append(morphs[0])
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[1:]))
+    del morphs[:]
+  elif word_morphs_str == '봐야 아/EC + 보/VX + 아야/EC + 하/VX' \
+      and forward_pairs and forward_pairs[-1].morphs[-1].tag == 'VV' \
+      and backward_pairs and str(backward_pairs[0].morphs[0]) == '겠/EP':
+    forward_pairs[-1].morphs.append(morphs[0])
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[1:-1]))
+    backward_pairs[0].morphs.insert(0, morphs[-1])
+    del morphs[:]
+  elif uni_word in [u'해졌', u'해봤', u'해했', u'해봐', u'해줬', u'해줘', u'해와', u'해져'] and len(morphs) == 4:
+    sandwich_pairs.append(WordStrMorphsPair(uni_word[0], morphs[:2]))
+    sandwich_pairs.append(WordStrMorphsPair(uni_word[1], morphs[2:]))
+    del morphs[:]
+  elif word_morphs_str == '잖 이/VCP + 지/EC + 않/VX' \
+      and forward_pairs and forward_pairs[-1].morphs[-1].tag.startswith('N'):
+    forward_pairs[-1].morphs.append(morphs[0])
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[1:]))
+    del morphs[:]
+  elif word_morphs_str in ['해서 하/VV + 아서/EC + 이/VCP', '해서 하/XSV + 아서/EC + 이/VCP',
+      '해서 하/VX + 아서/EC + 이/VCP', '서] 아서/EC+이/VCP'] \
+      and backward_pairs and backward_pairs[0].morphs[0].tag.startswith('E'):
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[:-1]))
+    backward_pairs[0].morphs.insert(0, morphs[-1])
+    del morphs[:]
+  elif word_morphs_str in ['서 아서/EC + 이/VCP'] \
+      and backward_pairs and backward_pairs[0].morphs[0].tag.startswith('E'):
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[:-1]))
+    backward_pairs[0].morphs.insert(0, morphs[-1])
+    del morphs[:]
+  else:
+    sim = calc_similarity(uni_word, unicode(''.join([morph.lex for morph in morphs]), 'UTF-8'))
+    if sim >= _SIMILARITY_MIN:
+      # string similarity is greater than threshold
+      sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs))
+      del morphs[:]
+    else:
+      raise AlignError()
+  return sandwich_pairs
+
+
+def proc_sw_only_word(word, forward_pairs, uni_word, backward_pairs):
+  """
+  process sandwich(remaining) (word, morphs) pair when word is empty and only single morpheme is left
+  @param  word            sejong.Word object
+  @param  forward_pairs   forward pair list
+  @param  uni_word        word
+  @param  backward_pairs  backward pair list
+  @return                 sandwich pair list. AlignError raised when error occurs
+  """
+  sandwich_pairs = []
+  if uni_word == u'.' and word.raw.endswith('.'):
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, [sejong_corpus.Morph.parse('./SF'),]))
+  elif uni_word == u'~' and (not backward_pairs or len(backward_pairs) == 1):
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, [sejong_corpus.Morph.parse('~/SO'),]))
+  elif uni_word == u',' and not backward_pairs:
+    sandwich_pairs.append(WordStrMorphsPair(uni_word, [sejong_corpus.Morph.parse('~/SP'),]))
+  elif uni_word == u'<pause/>':
+    pass
+  else:
+    raise AlignError()
+  return sandwich_pairs
+
+
+def proc_sw_only_morphs(forward_pairs, morphs, backward_pairs):
+  """
+  process sandwich(remaining) (word, morphs) pair when word is empty and only single morpheme is left
+  @param  forward_pairs   forward pair list
+  @param  morphs          morpheme list
+  @param  backward_pairs  backward pair list
+  @return                 sandwich pair list. AlignError raised when error occurs
+  """
+  sandwich_pairs = []
+  if not backward_pairs:
+    forward_pairs[-1].morphs.extend(morphs)
+    del morphs[:]
+  elif len(morphs) == 1:
+    morph = morphs[0]
+    morph_str = str(morph)
+    if morph_str in ['이/VCP', '하/VX'] and backward_pairs[0].morphs[0].tag.startswith('E'):
+      # '이' 긍정지정사나 '하' 보조용언 뒤에 어미가 나올 경우
+      backward_pairs[0].morphs.insert(0, morphs[0])
+      del morphs[:]
+    elif morph_str == '에/JKB' and backward_pairs[0].morphs[0].tag == 'JX':
+      # '에' 부사격조사 뒤에 보조사가 나올 경우
+      backward_pairs[0].morphs.insert(0, morphs[0])
+      del morphs[:]
+    elif morph_str == 'ᆯ/ETM' and forward_pairs[-1].morphs[-1].tag.startswith('V'):
+      # 'ㄹ' 관형형전성어미 앞에 용언이 나올 경우
+      forward_pairs[-1].morphs.append(morphs[0])
+      del morphs[:]
+    elif morph.tag in ['EC', 'EF'] and forward_pairs[-1].morphs[-1].tag.startswith('V'):
+      # 연결어미나 종결어미 앞에 용언이 나올 경우
+      forward_pairs[-1].morphs.append(morphs[0])
+      del morphs[:]
+    elif morph.tag.startswith('XS'):
+      # append suffixes to the end of forward pair list
+      forward_pairs[-1].morphs.append(morphs[0])
+      del morphs[:]
+    else:
+      raise AlignError()
+  else:
+    morphs_str = ' + '.join([str(morph) for morph in morphs])
+    if morphs_str == '(/SS + 대북/NNG + (/SS + 대북/NNG + )/SS + )/SS' and forward_pairs[-1].word_str == u'대북':
+      del morphs[:]
+    elif morphs_str == '(/SS + 동경/NNP + )/SS' and forward_pairs[-1].word_str == u'도쿄':
+      del morphs[:]
+    else:
+      raise AlignError()
+  return sandwich_pairs
+
+
+def proc_sandwich(word, forward_pairs, uni_word, morphs, backward_pairs):
   """
   process sandwich(remaining) (word, morphs) pair
+  @param  word            sejong.Word object
   @param  forward_pairs   forward pair list
   @param  uni_word        word
   @param  morphs          morpheme list
@@ -274,85 +446,11 @@ def proc_sandwich(forward_pairs, uni_word, morphs, backward_pairs):
   """
   sandwich_pairs = []
   if uni_word and morphs:
-    morphs_str = ' + '.join([str(morph) for morph in morphs])
-    word_morphs_str = '%s %s' % (uni_word.encode('UTF-8'), morphs_str)
-    if word_morphs_str in _CORRECT_ALIGNS:
-      sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs))
-      del morphs[:]
-    elif word_morphs_str in ['해야 하/VV + 아야/EC + 하/VX', '해야 하/XSV + 아야/EC + 하/VX'] and \
-        backward_pairs and str(backward_pairs[0].morphs[0]) == '겠/EP':
-      sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[:-1]))
-      backward_pairs[0].morphs.insert(0, morphs[-1])
-      del morphs[:]
-    elif word_morphs_str == '야 아야/EC + 하/VX' and backward_pairs and str(backward_pairs[0].morphs[0]) == '겠/EP':
-      sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[:-1]))
-      backward_pairs[0].morphs.insert(0, morphs[-1])
-      del morphs[:]
-    elif word_morphs_str in ['졌 어/EC + 지/VX + 었/EP', '져 어/EC + 지/VX + 어/EC'] \
-        and forward_pairs and forward_pairs[-1].morphs[-1].tag == 'VV':
-      forward_pairs[-1].morphs.append(morphs[0])
-      sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[1:]))
-      del morphs[:]
-    elif word_morphs_str in ['왔 아/EC + 오/VX + 았/EP', '왔 어/EC + 오/VX + 았/EP'] \
-        and forward_pairs and forward_pairs[-1].morphs[-1].tag == 'VV':
-      forward_pairs[-1].morphs.append(morphs[0])
-      sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[1:]))
-      del morphs[:]
-    elif uni_word in [u'해졌', u'해봤', u'해했', u'해봐', u'해줬', u'해줘', u'해와'] and len(morphs) == 4:
-      sandwich_pairs.append(WordStrMorphsPair(uni_word[0], morphs[:2]))
-      sandwich_pairs.append(WordStrMorphsPair(uni_word[1], morphs[2:]))
-      del morphs[:]
-    elif word_morphs_str  == '봤 아/EC + 보/VX + 았/EP' and forward_pairs and forward_pairs[-1].morphs[-1].tag == 'VV':
-      forward_pairs[-1].morphs.append(morphs[0])
-      sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs[1:]))
-      del morphs[:]
-    else:
-      sim = calc_similarity(uni_word, unicode(''.join([morph.lex for morph in morphs]), 'UTF-8'))
-      if sim >= _SIMILARITY_MIN:
-        # string similarity is greater than threshold
-        sandwich_pairs.append(WordStrMorphsPair(uni_word, morphs))
-        del morphs[:]
-      else:
-        raise AlignError()
+    sandwich_pairs = proc_sw_both_not_empty(forward_pairs, uni_word, morphs, backward_pairs)
   elif uni_word:
-    raise AlignError()
+    sandwich_pairs = proc_sw_only_word(word, forward_pairs, uni_word, backward_pairs)
   elif morphs:
-    if not backward_pairs:
-      forward_pairs[-1].morphs.extend(morphs)
-      del morphs[:]
-    elif len(morphs) == 1:
-      morph = morphs[0]
-      morph_str = str(morph)
-      if morph_str in ['이/VCP', '하/VX'] and backward_pairs[0].morphs[0].tag.startswith('E'):
-        # '이' 긍정지정사나 '하' 보조용언 뒤에 어미가 나올 경우
-        backward_pairs[0].morphs.insert(0, morphs[0])
-        del morphs[:]
-      elif morph_str == '에/JKB' and backward_pairs[0].morphs[0].tag == 'JX':
-        # '에' 부사격조사 뒤에 보조사가 나올 경우
-        backward_pairs[0].morphs.insert(0, morphs[0])
-        del morphs[:]
-      elif morph_str == 'ᆯ/ETM' and forward_pairs[-1].morphs[-1].tag.startswith('V'):
-        # 'ㄹ' 관형형전성어미 앞에 용언이 나올 경우
-        forward_pairs[-1].morphs.append(morphs[0])
-        del morphs[:]
-      elif morph.tag in ['EC', 'EF'] and forward_pairs[-1].morphs[-1].tag.startswith('V'):
-        # 연결어미나 종결어미 앞에 용언이 나올 경우
-        forward_pairs[-1].morphs.append(morphs[0])
-        del morphs[:]
-      elif morph.tag.startswith('XS'):
-        # append suffixes to the end of forward pair list
-        forward_pairs[-1].morphs.append(morphs[0])
-        del morphs[:]
-      else:
-        raise AlignError()
-    else:
-      morphs_str = ' + '.join([str(morph) for morph in morphs])
-      if morphs_str == '(/SS + 대북/NNG + (/SS + 대북/NNG + )/SS + )/SS' and forward_pairs[-1].word_str == u'대북':
-        del morphs[:]
-      elif morphs_str == '(/SS + 동경/NNP + )/SS' and forward_pairs[-1].word_str == u'도쿄':
-        del morphs[:]
-      else:
-        raise AlignError()
+    sandwich_pairs = proc_sw_only_morphs(forward_pairs, morphs, backward_pairs)
   return sandwich_pairs
 
 
@@ -369,7 +467,7 @@ def align(word):
   backward_pairs = []
   uni_word, morphs = backward_search(backward_pairs, uni_word, morphs)
   try:
-    sandwich_pairs = proc_sandwich(forward_pairs, uni_word, morphs, backward_pairs)
+    sandwich_pairs = proc_sandwich(word, forward_pairs, uni_word, morphs, backward_pairs)
   except AlignError, align_err:
     log_not_aligned(word.raw, forward_pairs, uni_word, morphs, backward_pairs)
     raise align_err
