e5648796414b35437a1f68d83780e3691c5fbc6e
krikit <krikit@naver.com>
2015-05-11 21:59:07 +0900

    make project source directory structure

---
+++ src/main/cpp/hanal/Trie.hpp
@@ -0,0 +1,24 @@
+/**
+ * @author     krikit(krikit@naver.com)
+ * @copyright  Copyright (C) 2014-2015, krikit. All rights reserved. BSD 2-Clause License
+ */
+
+
+#ifndef HANAL_TRIE_HPP
+#define HANAL_TRIE_HPP
+
+
+namespace hanal {
+
+
+/**
+ * trie for wide character string
+ */
+class Trie {
+};
+
+
+}
+
+
+#endif  // HANAL_TRIE_HPP
